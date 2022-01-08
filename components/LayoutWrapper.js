fbd2efafaab83446d345411c5f172e4272d4eade
kylegrover <kyle@kylegrover.com>
2022-01-08 00:48:31 -0800

    more logo tweaks, replace favicon

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -15,7 +15,7 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
-                <div className="mr-3">
+                <div className="mr-1">
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
