386507447a3cbd876548e0f0bdf2c9dcdd13ec39
kylegrover <kyle@kylegrover.com>
2022-01-09 15:15:13 -0800

    images & wording

--- pages/links.js
+++ pages/links.js
@@ -10,10 +10,10 @@ export default function Projects() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
+            Links
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            H=N Community resources and links
           </p>
         </div>
         <div className="container py-12">
