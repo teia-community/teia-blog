826e48135b0515da5e3910999b91da1e3c947526
kylegrover <kyle@kylegrover.com>
2022-01-12 12:56:54 -0800

    trying other discourse embed config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
+  default-src 'self' *.hencommunity.quest;
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.hencommunity.quest;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
