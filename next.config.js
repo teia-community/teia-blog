fe518b7c869e9d674629869be126618f3d8ad9c9
kylegrover <kyle@kylegrover.com>
2022-01-11 17:48:05 -0800

    allow discourse frame-src

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
-  frame-src giscus.app
+  frame-src giscus.app discourse.hencommunity.quest
 `
 
 const securityHeaders = [
