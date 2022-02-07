8c606f68873334ea4ae07909b4a90ddd132d14b3
Kyle Grover <kyle@kylegrover.com>
2022-02-07 14:43:40 -0800

    add notion link

--- data/linksData.js
+++ data/linksData.js
@@ -24,6 +24,12 @@ const linksData = [
     href: 'https://discourse.hencommunity.quest',
   },
   {
+    title: 'TEIA-COMMUNITY Notion',
+    description: `A public collection of resources and documentation.`,
+    imgSrc: '/static/images/logo.png',
+    href: 'https://teia-community.notion.site/teia-community/TEIA-COMMUNITY-3a21f2ddd52b40069c7809b85a7ffbf3',
+  },
+  {
     title: 'Submit A Link',
     description: `Is there an important link we're missing? This is a community powered project and a community powered website, add a link by submitting a PR to our GitHub (it's easy!), or create an issue to provide the info for someone else to add it.`,
     imgSrc: '/static/images/github-rectangle.jpg',
