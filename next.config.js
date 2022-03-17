ff71be892d7150f139fe8513330a8f13aa99f23c
Mel Massadian <melmassadian@gmail.com>
2022-03-17 16:53:10 +0100

    fix: ⚡️ Next.js Image API

--- next.config.js
+++ next.config.js
@@ -58,6 +58,7 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
   },
+  images: { loader: 'custom' },
   async headers() {
     return [
       {
